174469a9a053bbad7e59a24e90774bfb310bcad9
hebaishi <hebaishi.husam@gmail.com>
2019-12-01 22:10:53 +0000

    Solve Day 1 problems

--- src/day_01/puzzle1.cpp
+++ src/day_01/puzzle1.cpp
@@ -1,3 +1,14 @@
+#include <fstream>
+#include <iostream>
+
 int main() {
+  std::ifstream input_file(CURRENT_DIR "/input1.txt");
+  std::string line;
+  size_t total = 0;
+  while (std::getline(input_file, line)){
+    size_t current = std::stoi(line);
+    total += static_cast<int>((current / 3) - 2);
+  }
+  std::cout << "total = " << total << std::endl;
   return 0;
 }
