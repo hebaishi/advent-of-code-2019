ee0c8b5ef91ee9587bf372cbb1e04366d7a088ee
hebaishi <hebaishi.husam@gmail.com>
2019-12-21 00:17:57 +0000

    Solve day7 part2

--- src/puzzles/src/day07.cpp
+++ src/puzzles/src/day07.cpp
@@ -39,7 +39,7 @@ TEST_CASE( "Day 7 solution part 1", "[day7]" ) {
 
   for (const auto& combination : combinations) {
     std::vector<emulator::Intcode> amplifiers;
-    for (auto _ : combination) {
+    for (size_t idx = 0 ; idx < combination.length() ; idx++) {
       amplifiers.emplace_back();
       amplifiers.back().SetInstructions(original_instructions);
     }
@@ -67,35 +67,48 @@ TEST_CASE( "Day 7 solution part 1", "[day7]" ) {
 }
 
 TEST_CASE( "Day 7 solution, part 2", "[day7]" ) {
-//  auto combinations = GetCombinations({"5", "6", "7", "8", "9"});
-//  REQUIRE(combinations.size() == 120);
+  auto combinations = GetCombinations({"5", "6", "7", "8", "9"});
+  REQUIRE(combinations.size() == 120);
+  auto original_instructions = io::ReadInstructions(CURRENT_DIR "/input7.txt");
+  std::set<int> outputs;
 
-//  auto original_instructions = io::ReadInstructions(CURRENT_DIR "/input7.txt");
-//  std::set<int> outputs;
-//  for (const auto& combination : combinations) {
-//    std::list<int64_t> inputs;
-//    int64_t current_input = 0;
-//    for (int64_t rep = 0 ; rep < 20 ; rep++) {
-//      for (auto c : combination) {
-//        emulator::Intcode processor;
-//        auto instructions = original_instructions;
-//        int phase_setting = c - '0';
-//        inputs = {phase_setting, current_input};
-//        auto input_it = inputs.begin();
-//        processor.SetInputCallback([&input_it]() -> int64_t {
-//          auto value = *input_it++;
-//          return value;
-//        });
-//        processor.SetOutputCallback([&current_input, &inputs](int64_t value) {
-//          if (inputs.size() == 2)
-//            current_input = value;
-//          inputs.push_back(value);
-//        });
-//        processor.ProcessInstructions(instructions);
-//      }
-//      outputs.insert(current_input);
-//    }
-//  }
+  for (const auto& combination : combinations) {
+    std::vector<emulator::Intcode> amplifiers;
+    for (size_t idx = 0 ; idx < combination.length() ; idx++) {
+      amplifiers.emplace_back();
+      amplifiers.back().SetInstructions(original_instructions);
+    }
 
-//  REQUIRE(*outputs.rbegin() == 21000);
+    std::list<int64_t> inputs;
+    int64_t current_input = 0;
+    int num_iterations = 0;
+    while(num_iterations >= 0) {
+      for (size_t index = 0 ; index < combination.length() ; index++) {
+        auto& current_amplifier = amplifiers[index];
+        int phase_setting = combination[index] - '0';
+        auto state = current_amplifier.Run();
+        if (num_iterations == 0) {
+          assert(state == emulator::State::AwaitingInput);
+          current_amplifier.ProvideInput(phase_setting);
+          state = current_amplifier.Run();
+        }
+        if (state == emulator::State::AwaitingInput) {
+          current_amplifier.ProvideInput(current_input);
+          state = current_amplifier.Run();
+        }
+        if (state == emulator::State::ProvidedOutput) {
+          current_input = current_amplifier.GetOutput();
+        } else if (state == emulator::State::Halted && index == 4) {
+          num_iterations = -6;
+          break;
+        }
+
+      }
+
+      num_iterations++;
+    }
+    outputs.insert(current_input);
+  }
+
+  REQUIRE(*outputs.rbegin() == 61379886);
 }
