9a781b55a1007aeea8773dd825f1e43eb9cdc289
hebaishi <hebaishi.husam@gmail.com>
2019-12-22 14:29:34 +0000

    Refactor day 9 puzzle

--- src/puzzles/src/day09.cpp
+++ src/puzzles/src/day09.cpp
@@ -5,28 +5,27 @@
 #include <catch/catch.hpp>
 
 #include <iostream>
+#include <vector>
 
-TEST_CASE( "Day 9 solution part 1", "[day9]" ) {
+struct Iteration {
+  int64_t input, output;
+};
+
+TEST_CASE( "Day 9 solution parts 1 + 2", "[day9]" ) {
   auto original_instructions = io::ReadInstructions(CURRENT_DIR "/input9.txt");
-  emulator::Intcode processor;
-  processor.SetInstructions(original_instructions);
-  auto state = processor.Run();
-  if (state == emulator::State::AwaitingInput) {
-    processor.ProvideInput(1);
-  }
-  state = processor.Run();
-  REQUIRE(processor.GetOutput() == 2682107844);
-}
+  std::vector<Iteration> iterations = {
+    {1, 2682107844},
+    {2, 34738}
+  };
 
-TEST_CASE( "Day 9 solution part 2", "[day9]" ) {
-  auto original_instructions = io::ReadInstructions(CURRENT_DIR "/input9.txt");
-  emulator::Intcode processor;
-  processor.SetInstructions(original_instructions);
-  auto state = processor.Run();
-  if (state == emulator::State::AwaitingInput) {
-    processor.ProvideInput(2);
+  for (const auto& iteration : iterations) {
+    emulator::Intcode processor;
+    processor.SetInstructions(original_instructions);
+    auto state = processor.Run();
+    if (state == emulator::State::AwaitingInput) {
+      processor.ProvideInput(iteration.input);
+    }
+    state = processor.Run();
+    REQUIRE(processor.GetOutput() == iteration.output);
   }
-  state = processor.Run();
-  REQUIRE(processor.GetOutput() == 34738);
 }
-
